6a3d1db693be3cec6cc4f040aa0a806606ae6fe1
Nino <125403389+nino-clmity@users.noreply.github.com>
2025-04-28 16:38:36 -0300

    commit-not found e perguntas frequentes

---
+++ tracsy/src/app/not-found.tsx
@@ -0,0 +1,9 @@
+function error404() {
+    return (
+        <div className="p-100">
+            <h1 className="font-bold text-[30px]">404 | Esta página não foi encontrada</h1>
+        </div>
+    )
+}
+
+export default error404
--- tracsy/src/app/perguntasFrequentes/page.tsx
+++ tracsy/src/app/perguntasFrequentes/page.tsx
@@ -1,9 +1,38 @@
-
+import {
+    Accordion,
+    AccordionContent,
+    AccordionItem,
+    AccordionTrigger,
+} from "@/components/ui/accordion"
 
 function PerguntasFrequentesPage() {
     return (
-        <div>
-            <h1>Perguntas Frequentes</h1>
+        <div className="w-[40%]">
+            <div className="text-center p-5 font-bold text-[25px]">
+                <h1>Perguntas Frequentes</h1>
+            </div>
+            <div className="w-full">
+                <Accordion type="single" collapsible className="w-full">
+                    <AccordionItem value="item-1">
+                        <AccordionTrigger>Como cadastro um desaparecido?</AccordionTrigger>
+                        <AccordionContent>
+                            Você deve fazer login e clicar em cadastrar desaparecido no nosso header.
+                        </AccordionContent>
+                    </AccordionItem>
+                    <AccordionItem value="item-2">
+                        <AccordionTrigger>Pergunta2?</AccordionTrigger>
+                        <AccordionContent>
+                            resposta 2
+                        </AccordionContent>
+                    </AccordionItem>
+                    <AccordionItem value="item-3">
+                        <AccordionTrigger>Pergunta3?</AccordionTrigger>
+                        <AccordionContent>
+                            resposta 3
+                        </AccordionContent>
+                    </AccordionItem>
+                </Accordion>
+            </div>
         </div>
     );
 }
