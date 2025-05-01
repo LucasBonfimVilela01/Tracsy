d80dbf41049bfa79232611f3660e67503ffab896
Lucas <lucasbvilela01@gmail.com>
2025-05-01 02:51:43 -0300

    Commit 4.1-Atualização da página de erro e links de mapa

--- tracsy/src/app/not-found.tsx
+++ tracsy/src/app/not-found.tsx
@@ -1,9 +1,15 @@
-function error404() {
+"use client";
+import { usePathname } from "next/navigation";
+
+function Error404() {
+    const pathname = usePathname();
     return (
         <div className="p-100">
-            <h1 className="font-bold text-[30px]">404 | Esta página não foi encontrada</h1>
+            <h1 className="font-bold text-[30px]">
+                404 | Esta página "{pathname}" não foi encontrada
+            </h1>
         </div>
-    )
+    );
 }
 
-export default error404
+export default Error404;
--- tracsy/src/app/page.tsx
+++ tracsy/src/app/page.tsx
@@ -20,7 +20,7 @@ export default function Home() {
         idade="20" 
         descricao="Pessoa que desapareceu"
         contato="email@email.com.br" 
-        mapaSrc="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d8298.6590190901!2d-46.564763887181876!3d-23.53738934794026!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94cd5228bab1966d%3A0x9905169ef1825576!2sUbatuba%2C%20SP!5e0!3m2!1spt-BR!2sbr!4v1746074635546!5m2!1spt-BR!2sbr"
+        mapaSrc="https://www.google.com/maps/embed?pb=!1m10!1m8!1m3!1d228.78387064553442!2d-45.080285975644095!3d-23.44090876514013!3m2!1i1024!2i768!4f13.1!5e0!3m2!1spt-BR!2sbr!4v1746078171764!5m2!1spt-BR!2sbr"
         />
         <CardFeed 
         fotoSrc="/placeHolderImage.png" 
@@ -28,7 +28,7 @@ export default function Home() {
         idade="20" 
         descricao="Pessoa que desapareceu"
         contato="email@email.com.br" 
-        mapaSrc="Link do mapa"
+        mapaSrc="https://www.google.com/maps/embed?pb=!1m10!1m8!1m3!1d228.78387064553442!2d-45.080285975644095!3d-23.44090876514013!3m2!1i1024!2i768!4f13.1!5e0!3m2!1spt-BR!2sbr!4v1746078171764!5m2!1spt-BR!2sbr"
         />
     </div>
   );
